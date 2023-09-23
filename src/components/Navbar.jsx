e657e4d694fac5d28f442e5370319c90be85fc67
sehzam <sehzam@hotmail.com>
2023-09-24 01:28:03 +0200

    navLinks classNames cascade effect for active-hove

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,14 @@ const Navbar = () => {
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
-            <li className="" key={link.id}>
+            <li
+              key={link.id}
+              className={`${active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
