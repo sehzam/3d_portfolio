33aa8a9ccc0e6fd02e7abd9f103273bfdacd3c26
sehzam <sehzam@hotmail.com>
2023-09-24 01:01:39 +0200

    add navbar brand

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,9 @@ const Navbar = () => {
             window.scrollTo(0, 0)
           }}>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Kadir <span className="sm:block hidden">| 3D Developer Portfolio</span></p>
         </Link>
+        <p className="text-red-500">menu</p>
       </div>
     </nav>
   )
