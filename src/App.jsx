40f5d2149aa48fad8f3c0e02e3f52e198c1e29d4
sehzam <sehzam@hotmail.com>
2023-09-24 00:03:06 +0200

    app imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
 const App = () => {
 
   return (
-    <div>
-      3D Developer Portfolio 
+    <BrowserRouter>
+    <div className="">
+      
     </div>
+    </BrowserRouter>
   )
 }
 
