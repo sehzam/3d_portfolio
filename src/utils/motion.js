fa62e1bf96db593ead3cfd3a06a7846203e29b86
sehzam <sehzam@hotmail.com>
2023-09-23 21:11:41 +0200

    motion js - slideIn

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -55,3 +55,22 @@ export const zoomIn = (delay, duration) => {
         },
     };
 };
+
+export const slideIn = (direction, type, delay, duration) => {
+    return {
+        hidden: {
+            x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+            y: direction === "up" ? "100%" : direction === "down" ? "100" : 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            transition: {
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
+};
