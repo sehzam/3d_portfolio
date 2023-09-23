762dd9cfecf2a362aeadc1ceec1be41a1c2db020
sehzam <sehzam@hotmail.com>
2023-09-24 00:19:46 +0200

    cascading frame - navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,11 @@ import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex-justify-between items-center max-w-7xl mx-auto">
+
+      </div>
+    </nav>
   )
 }
 
