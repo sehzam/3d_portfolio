a5ff2cc6ef73757ffef33a5acf74da726e4ea4ef
sehzam <sehzam@hotmail.com>
2023-09-23 20:45:37 +0200

    motion js - zoomIn

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -36,3 +36,22 @@ export const fadeIn = (directioin, type, delay, duration) => {
         },
     };
 };
+
+export const zoomIn = (delay, duration) => {
+    return {
+        hidden: {
+            scale: 0,
+            opacity: 0,
+        },
+        show: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+                type: "tween",
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
+};
