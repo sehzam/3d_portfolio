48290029c2607a2334c303369aa57b393315efd6
sehzam <sehzam@hotmail.com>
2023-09-24 20:40:35 +0200

    add capsule div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,11 @@ const Hero = () => {
       <ComputersCanvas />
 
       <div className="absolute xs-bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
 
+          </div>
+        </a>
       </div>
     </section>
   )
