c04917b25cefb2e47d2d422b7e2f40de3d13242e
sehzam <sehzam@hotmail.com>
2023-09-25 01:08:47 +0200

    add motion section - cascade

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -11,6 +11,7 @@ const SectionWrapper = (Component, idName) =>
                 initial="hidden"
                 whileInView={"show"}
                 viewport={{ once: true, amount: 0.25 }}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
             >
                 <Component />
             </motion.section>
