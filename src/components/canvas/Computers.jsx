a5b3abd111e7a06dfcdeaef0f0f536ab56e08665
sehzam <sehzam@hotmail.com>
2023-09-24 18:16:04 +0200

    install three - add scene - hemisphere

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,8 +5,12 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+        groundColor="black" />
+    </mesh>
   )
 }
 
