1cc40bb8c126c3401959ac2e49a99253cbfd0a09
sehzam <sehzam@hotmail.com>
2023-09-24 19:16:59 +0200

    add spotLight - positioning

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,6 +12,9 @@ const Computers = () => {
       <hemisphereLight intensity={0.15}
         groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
