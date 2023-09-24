d0d0dec1052d4a7ad190805ceeee6ec5043b4c91
sehzam <sehzam@hotmail.com>
2023-09-25 00:12:13 +0200

    add motion div - variants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,11 @@ import { fadeIn, textVariant } from '../utils/motion'
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      >
 
+      </motion.div>
     </Tilt>
   )
 }
