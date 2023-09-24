4676e8d8866f403a4297331fafbfa866fdce5976
sehzam <sehzam@hotmail.com>
2023-09-24 16:36:03 +0200

    container with cascading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,9 @@ import { ComputersCanvas } from './canvas/index'
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
 
+      </div>
     </section>
   )
 }
