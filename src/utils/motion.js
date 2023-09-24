9b1f93b527964f1f500914bf6200cd7c828fb0e5
sehzam <sehzam@hotmail.com>
2023-09-24 23:45:32 +0200

    motion.js - fix of false written code ???

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -13,14 +13,14 @@ export const textVariant = (delay) => {
                 delay: delay,
             },
         },
-    };
-};
+    }
+}
 
-export const fadeIn = (directioin, type, delay, duration) => {
+export const fadeIn = (direction, type, delay, duration) => {
     return {
         hidden: {
             x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-            y: direction === "up" ? 100 : diretion === "down" ? -100 : 0,
+            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
             opacity: 0,
         },
         show: {
@@ -34,8 +34,8 @@ export const fadeIn = (directioin, type, delay, duration) => {
                 ease: "easeOut",
             },
         },
-    };
-};
+    }
+}
 
 export const zoomIn = (delay, duration) => {
     return {
@@ -53,8 +53,8 @@ export const zoomIn = (delay, duration) => {
                 ease: "easeOut",
             },
         },
-    };
-};
+    }
+}
 
 export const slideIn = (direction, type, delay, duration) => {
     return {
@@ -72,8 +72,8 @@ export const slideIn = (direction, type, delay, duration) => {
                 ease: "easeOut",
             },
         },
-    };
-};
+    }
+}
 
 export const staggerContainer = (staggerChildren, delayChildren) => {
     return {
@@ -84,5 +84,5 @@ export const staggerContainer = (staggerChildren, delayChildren) => {
                 delayChildren: delayChildren || 0,
             },
         },
-    };
-};
+    }
+}
