255a2b32c3622d0e756a6b265bae8d39cafaa870
sehzam <sehzam@hotmail.com>
2023-09-24 21:53:01 +0200

    add isMobile parameter to model in canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -62,7 +62,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
