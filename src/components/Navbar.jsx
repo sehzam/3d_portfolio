6069b28505e84b451382d5e8dc223b603e7d58cf
sehzam <sehzam@hotmail.com>
2023-09-24 02:03:16 +0200

    navbar menu frame

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,23 @@ const Navbar = () => {
           />
 
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-
+            <ul className="list-none hidden justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title
+                    ? "text-white"
+                    : "text-secondary"
+                    } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
