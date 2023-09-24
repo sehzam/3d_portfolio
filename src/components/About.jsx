467a05fa28b80710226ff1c53d37bfbcef8aa2bd
sehzam <sehzam@hotmail.com>
2023-09-25 00:05:54 +0200

    add ServiceCard with parameters

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,9 @@ const About = () => {
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
-        { }
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
   )
