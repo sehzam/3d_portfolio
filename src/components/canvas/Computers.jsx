e75b747532188996b31862dbc14f9b84460d2372
sehzam <sehzam@hotmail.com>
2023-09-24 19:12:34 +0200

    add wrap model in canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,8 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('/desktop_pc/scene.gltf')
+
   return (
     <mesh>
       <hemisphereLight intensity={0.15}
@@ -27,10 +28,17 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas
