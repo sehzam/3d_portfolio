d9073ede1cf46485b8466ac154397ec74b7c461c
sehzam <sehzam@hotmail.com>
2023-09-24 22:45:38 +0200

    modify scale of primitive model isMobile diff

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('/desktop_pc/scene.gltf')
 
   return (
@@ -22,7 +22,7 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
+        scale={isMobile ? 0.7 : 0.75}
         position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
