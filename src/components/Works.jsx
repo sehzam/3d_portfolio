113a6ea7ae375a0a77a6259dc1e5a2334cdf4e86
sehzam <sehzam@hotmail.com>
2023-09-25 16:31:07 +0200

    add ProjectCard - parameter move throught

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,6 +6,10 @@ import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+
+}
+
 const Works = () => {
   return (
     <>
