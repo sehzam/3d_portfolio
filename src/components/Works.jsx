90b577a91cae07ab4f281da629abc370e56c8851
sehzam <sehzam@hotmail.com>
2023-09-25 16:24:54 +0200

    add mapped projects into div - ProjectCard declare

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -20,10 +20,17 @@ const Works = () => {
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-
         >
-
+          Following projects showcases my skills and experience through real-wrld examples of my work.
+          Each project is vriefly described with links to code repositories and live demos in it.
+          It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard />
+        ))}
       </div>
     </>
   )
