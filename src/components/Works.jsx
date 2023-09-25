d298ab87f5c1b7211dd7a2e8d047f29876cabc93
sehzam <sehzam@hotmail.com>
2023-09-25 16:33:22 +0200

    add motion div - ProjectCard

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,7 +7,11 @@ import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div>
 
+    </motion.div>
+  )
 }
 
 const Works = () => {
@@ -26,7 +30,7 @@ const Works = () => {
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through real-wrld examples of my work.
-          Each project is vriefly described with links to code repositories and live demos in it.
+          Each project is briefly described with links to code repositories and live demos in it.
           It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
