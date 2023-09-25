bfe2577e6878141b6d529a1519aefc76ddade8eb
sehzam <sehzam@hotmail.com>
2023-09-25 16:20:10 +0200

    add parameter - cascade

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,6 +19,7 @@ const Works = () => {
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
 
         >
 
