856f60d1371ac27af411e7f528d3c07eb4ea2424
sehzam <sehzam@hotmail.com>
2023-09-25 17:19:14 +0200

    add div for tags

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -40,6 +40,9 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+
+        </div>
       </Tilt>
     </motion.div>
   )
