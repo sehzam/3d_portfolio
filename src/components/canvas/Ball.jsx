d56f24c83cc0cfd377e46a91857206a9cd70ab60
sehzam <sehzam@hotmail.com>
2023-09-25 11:47:13 +0200

    add OrbitControls parameter - minPolarAngle

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -26,6 +26,7 @@ const BallCanvas = ({ icon }) => {
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
       </Suspense>
     </Canvas>
