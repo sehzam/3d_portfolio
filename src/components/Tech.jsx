a70d293bf3a7d067a0b6efc385f050f46a1f5d13
sehzam <sehzam@hotmail.com>
2023-09-25 11:18:46 +0200

    add div cascade - for mapping technologies

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,9 @@ import { technologies } from '../constants'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+
+    </div>
   )
 }
 
