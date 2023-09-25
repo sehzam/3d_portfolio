99a5dc7f62b2155b19238706602eb2b9bf9b658e
sehzam <sehzam@hotmail.com>
2023-09-25 17:41:13 +0200

    add div for feedbacks - cascade

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,7 +6,11 @@ import { testimonials } from '../constants'
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+
+      </div>
+    </div>
   )
 }
 
