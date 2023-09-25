af1467fd6830c23b2e8caa8f4b6331392f3b9442
sehzam <sehzam@hotmail.com>
2023-09-25 16:37:03 +0200

    add tilt and there options

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,7 +9,15 @@ import { fadeIn, textVariant } from '../utils/motion'
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      test
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+      >
+
+      </Tilt>
     </motion.div>
   )
 }
