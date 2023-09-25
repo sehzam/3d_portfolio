409dbb3649511b1aca7eaed535c4eba71436dd93
sehzam <sehzam@hotmail.com>
2023-09-25 11:20:59 +0200

    add key - cascade

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,11 @@ import { technologies } from '../constants'
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
 
+        </div>
+      ))}
     </div>
   )
 }
