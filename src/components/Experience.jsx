61dd8548f0d7d9384e70abe5a697c7251c0fdbb9
sehzam <sehzam@hotmail.com>
2023-09-25 10:29:36 +0200

    add motion div - title paragraph - cascade

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,7 +12,12 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-
+        <p className={styles.sectionSubText}>
+          What I have done so far
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Work Experience.
+        </h2>
       </motion.div>
     </>
   )
