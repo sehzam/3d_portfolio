3a52bf7975edeec021948b5639d61a9d5b198bae
sehzam <sehzam@hotmail.com>
2023-09-25 16:44:22 +0200

    add div - projects frame

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,7 +18,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
-
+          <img src={image} alt={name} />
         </div>
       </Tilt>
     </motion.div>
