436a85b9bc84c61aaf2271c279df925f1167b81f
sehzam <sehzam@hotmail.com>
2023-09-25 16:54:04 +0200

    add button inside the div - cascade

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -26,6 +26,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
 
             </div>
