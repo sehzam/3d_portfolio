4d90f75db6f6b50a0dd3db3255b31acd21491480
sehzam <sehzam@hotmail.com>
2023-09-26 19:44:39 +0200

    add  testimonials footer - cascade

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -11,13 +11,13 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
   >
     <p className="text-white font-black text-[48px]">"</p>
     <div className="mt-1">
-      <p>{testimonial}</p>
+      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
       <div className="mt-7 flex justify-between items-center gap-1">
         <div className="flex-1 flex flex-col">
-          <p>
-            <span>@</span> {name}
+          <p className="text-white font-medium text-[16px]">
+            <span className="blue-text-gradient">@</span> {name}
           </p>
-          <p>
+          <p className="mt-1 text-secondary text-[12px]">
             {designation} of {company}
           </p>
 
