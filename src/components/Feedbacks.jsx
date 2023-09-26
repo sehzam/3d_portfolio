83178acff3197943b83a0555248e9a615f0c97d9
sehzam <sehzam@hotmail.com>
2023-09-26 19:02:18 +0200

    add title p - feedbacks

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -8,7 +8,10 @@ const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
-
+        <motion.div variants={textVariant()}>
+          <p>What others say</p>
+          <h2>Testimonials.</h2>
+        </motion.div>
       </div>
     </div>
   )
