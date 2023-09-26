2a7b01dcd9da3ecc649f88c40c33fdeac0e81c8b
sehzam <sehzam@hotmail.com>
2023-09-26 19:04:19 +0200

    add cascade - feedbacks

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -9,8 +9,8 @@ const Feedbacks = () => {
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <motion.div variants={textVariant()}>
-          <p>What others say</p>
-          <h2>Testimonials.</h2>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
     </div>
