1f05e8ac2eb45f6f671a5e3fe678d9824fba07ef
sehzam <sehzam@hotmail.com>
2023-09-27 02:46:21 +0200

    add useRef const - form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,12 @@ import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
 const Contact = () => {
+  const formRef = useRef()
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
   return (
     <div>Contact</div>
   )
