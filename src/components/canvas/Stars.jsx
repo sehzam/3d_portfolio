41e1015ffbccadf37e6107d97b139af07335f5c3
Kadir Duran <sehzam@hotmail.com>
2023-09-28 13:09:31 +0200

    add ref and sphere

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,7 +4,9 @@ import { Points, PointMaterial, Preload } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
+  const ref = useRef()
 
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
   return (
     <div>Stars</div>
   )
