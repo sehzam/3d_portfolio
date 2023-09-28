db6d180870e367b49043e7f64ab38fc344162a7c
Kadir Duran <sehzam@hotmail.com>
2023-09-28 12:06:03 +0200

    add camera settings

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,8 +5,14 @@ import { Canvas } from "react-three-fiber"
 import CanvasLoader from '../Loader'
 
 const Earth = () => {
+  const earth = useGLTF('/planet/scene.gltf')
   return (
-    <div>Earth</div>
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    />
   )
 }
 
@@ -15,7 +21,12 @@ const EarthCanvas = () => {
     shadows
     frameloop="demand"
     gl={{ preserveDrawingBuffer: true }}
-    camera={{}}
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6]
+    }}
   >
     <Suspense fallback={<CanvasLoader />}>
       <OrbitControls
