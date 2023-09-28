2ac98d6226c74c9b317b7fb00fbc2dde132d060f
Kadir Duran <sehzam@hotmail.com>
2023-09-28 13:02:27 +0200

    add canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,10 +3,16 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    
+  )
+}
+
+export default StarsCanvas
