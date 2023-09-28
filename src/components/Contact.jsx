ba019209f7abfa7126ce186fced6cea48dabf2e0
Kadir Duran <sehzam@hotmail.com>
2023-09-28 10:10:27 +0200

    add form with label

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,6 +25,14 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+
+        </form>
       </motion.div>
     </div>
   )
