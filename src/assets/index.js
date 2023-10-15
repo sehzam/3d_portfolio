dc9cad4516ba980df2e0aed0437f8e68e1cde645
Kadir Duran <sehzam@hotmail.com>
2023-10-15 16:48:01 +0200

    add - import new icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,64 +1,148 @@
-import logo from "./logo.png"
-import backend from "./backend.png"
-import creator from "./creator.png"
-import mobile from "./mobile.png"
-import web from "./web.png"
-import github from "./github.png"
-import menu from "./menu.svg"
-import close from "./close.svg"
-
+// LOGO
+import logo from "./icon/logo.png"
+// TECHNOLOGY
+import bash from "./tech/bash.png"
 import css from "./tech/css.png"
 import docker from "./tech/docker.png"
-import figma from "./tech/figma.png"
+import gimp from "./tech/gimp.png"
 import git from "./tech/git.png"
+import github from "./tech/github.png"
 import html from "./tech/html.png"
+import java from "./tech/java.png"
 import javascript from "./tech/javascript.png"
+import kali from "./tech/kali.png"
+import matplotlib from "./tech/matplotlib.png"
 import mongodb from "./tech/mongodb.png"
 import nodejs from "./tech/nodejs.png"
+import numpy from "./tech/numpy.png"
+import office from "./tech/office.png"
+import pandas from "./tech/pandas.png"
+import php from "./tech/php.png"
+import powershell from "./tech/powershell.png"
+import python from "./tech/python.png"
 import reactjs from "./tech/reactjs.png"
 import redux from "./tech/redux.png"
+import scikitlearn from "./tech/scikitlearn.png"
+import sql from "./tech/sql.png"
 import tailwind from "./tech/tailwind.png"
+import threejs from "./tech/threejs.png"
 import typescript from "./tech/typescript.png"
-import threejs from "./tech/threejs.svg"
+import vscode from "./tech/vscode.png"
+import windows from "./tech/windows.png"
+// NEW TECHNOLOGY
+import expressjs from './tech/expressjs.png'
+import nextjs from './tech/nextjs.png'
+import framer from './tech/framer.png'
+import emotion from './tech/emotion.png'
+import chakra from './tech/chakra.png'
+// COMPANY
+import gfn from "./company/gfn.png"
+import ihk from "./company/ihk.png"
+import oracle from "./company/oracle.png"
+import spiced from "./company/spiced.png"
+import we from "./company/we.png"
+// PAGES
+import datil from "./page/datil.png"
+import trainervote from "./page/trainervote.png"
+import tml from "./page/tml.png"
+import ali from "./page/ali.png"
+import ddp from "./page/ddp.png"
+import trix from "./page/trix.png"
+import purely from "./page/purely.png"
+import next from "./company/next.png"
+// ABOUT CARD
+import frontend from "./card/frontend.png"
+import backend from "./card/backend.png"
+import mobile from "./card/mobile.png"
+import data from "./card/data_analyst.png"
+import vrar from "./card/vr_ar.png"
+import threed from "./card/threed.png"
+import technician from "./card/technician.png"
+import admin from "./card/admin.png"
+// NAV ICON
+import hero from "./nav/hero.png"
+import about from "./nav/about.png"
+import contact from "./nav/contact.png"
+import experience from "./nav/experience.png"
+import feedback from "./nav/feedback.png"
+import technology from "./nav/technology.png"
+import work from "./nav/work.png"
+// OTHER
+import avatar from "./icon/avatar.png"
+import back from "./icon/back.png"
+import close from "./icon/close.png"
+import convert from "./icon/convert.png"
+import cv from "./icon/cv.png"
+import pdf from "./icon/pdf.png"
 
-import meta from "./company/meta.png"
-import shopify from "./company/shopify.png"
-import starbucks from "./company/starbucks.png"
-import tesla from "./company/tesla.png"
-
-import carrent from "./carrent.png"
-import jobit from "./jobit.png"
-import tripguide from "./tripguide.png"
-import ali from "./ali.png"
 
 export {
-  ali,
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+    cv,
+    avatar,
+    back,
+    close,
+    convert,
+    pdf,
+    vrar,
+    threed,
+    technician,
+    admin,
+    expressjs,
+    nextjs,
+    framer,
+    emotion,
+    chakra,
+    hero,
+    about,
+    contact,
+    experience,
+    feedback,
+    technology,
+    work,
+    data,
+    mobile,
+    backend,
+    frontend,
+    trainervote,
+    next,
+    purely,
+    ali,
+    ddp,
+    trix,
+    tml,
+    datil,
+    logo,
+    github,
+    bash,
+    css,
+    docker,
+    git,
+    gimp,
+    html,
+    java,
+    javascript,
+    kali,
+    matplotlib,
+    mongodb,
+    nodejs,
+    numpy,
+    office,
+    pandas,
+    php,
+    powershell,
+    python,
+    reactjs,
+    redux,
+    scikitlearn,
+    sql,
+    tailwind,
+    threejs,
+    typescript,
+    vscode,
+    windows,
+    gfn,
+    ihk,
+    oracle,
+    spiced,
+    we
 }
