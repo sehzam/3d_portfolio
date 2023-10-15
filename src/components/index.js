2868859ff2a7aa5dc5da7e91583d890ad09e1f2e
Kadir Duran <sehzam@hotmail.com>
2023-10-15 16:51:29 +0200

    add - index serving nodes

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,40 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+// Canvas
+import {
+  EarthCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+  LaptopCanvas,
+  TechCube
+} from './canvas'
+// Modal
+import ContactModal from './modal/ContactModal'
+import CvModal from './modal/CvModal'
+// Section
+import Hero from './section/Hero'
+import Navbar from './section/Navbar'
+import About from './section/About'
+import DataScience from './section/DataScience'
+import Technologies from './section/Technologies'
+import Experience from './section/Experience'
+import Works from './section/Works'
+import Feedbacks from './section/Feedbacks'
+import Contact from './section/Contact'
 
 export {
+  ContactModal,
+  CvModal,
   Hero,
   Navbar,
   About,
-  Tech,
+  DataScience,
+  Technologies,
   Experience,
   Works,
   Feedbacks,
-  Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
+  EarthCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+  LaptopCanvas,
+  TechCube,
+  Contact
 }
