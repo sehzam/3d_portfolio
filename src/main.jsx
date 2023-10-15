e7a7c8a7b11c0a348e0f995065a813d84b634788
Kadir Duran <sehzam@hotmail.com>
2023-10-15 16:47:17 +0200

    add - css split files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "tw-elements-react/dist/css/tw-elements-react.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-tippy/dist/tippy.css'
+import './styles/index.css'
+import './styles/animation.css'
+import './styles/font.css'
+import "./styles/gradient.css"
+import "./styles/gradientlist.css"
+import "./styles/class.css"
+import "./styles/custom.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
