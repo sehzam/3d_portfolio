252d5934764cb63521d3179232af2176bd4b606f
Kadir Duran <sehzam@hotmail.com>
2023-10-15 16:59:56 +0200

    add - canvas techCubbe

---
+++ src/components/canvas/components/TechCube.jsx
@@ -0,0 +1,26 @@
+import { useRef } from "react"
+import { useAnimationFrame } from "framer-motion"
+
+const TechCube = () => {
+    const ref = useRef(null)
+
+    useAnimationFrame((t) => {
+        const rotate = Math.sin(t / 10000) * 500
+        const y = (1 + Math.sin(t / 1000)) * -50
+        ref.current.style.transform = `scale(0.3) translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg)`
+    })
+
+    return (
+        <div className="container">
+            <div className="cube" ref={ref}>
+                <div className="side front" />
+                <div className="side left" />
+                <div className="side right" />
+                <div className="side top" />
+                <div className="side bottom" />
+                <div className="side back" />
+            </div>
+        </div>
+    )
+}
+export default TechCube
